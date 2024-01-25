24c74d8029cd3261c6621e8c977c2f0a9ccea42b
zelkovaria <hyein396@gmail.com>
2024-01-25 23:21:36 +0900

    feat: 작은 버튼 추가 - #4

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { Theme } from './styles/Theme';
 import GlobalStyle from './styles/GlobalStyle';
 import RoutesContainer from './components/RoutesContainer';
-import BigButton from './components/Button/BigButton';
+import SmallButton from './components/checkPage/SmallButton';
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
       <BrowserRouter>
-        <BigButton disabled={true}>지원하기</BigButton>
+        <SmallButton>확인하기</SmallButton>
         <RoutesContainer />
       </BrowserRouter>
     </ThemeProvider>
---
+++ src/components/checkPage/SmallButton.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const SmallButton = ({ children }) => {
+  return <Wrapper type="button">{children}</Wrapper>;
+};
+
+const Wrapper = styled.button`
+  border-radius: 40px;
+  font-size: 10px;
+  font-weight: 300;
+  padding: 9px 16px;
+  width: 74px;
+  height: 26px;
+  background-color: ${({ theme }) => theme.colors.MAIN_PINK};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.HOVER_BTN};
+  }
+
+  @media ${({ theme }) => theme.devices.DESKTOP} {
+    ${({ theme }) => theme.typographies.SMALL_BTN_TXT};
+    padding: 14px 48px;
+    border-radius: 40px;
+    width: 160px;
+    height: 54px;
+  }
+`;
+
+export default SmallButton;
