086d7eb9468c2d59244ba872cb4aa6de8ff2dd0c
zelkovaria <hyein396@gmail.com>
2024-01-26 01:31:42 +0900

    refactor: 확인용 코드 삭제 - #4

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Theme } from './styles/Theme';
 import GlobalStyle from './styles/GlobalStyle';
 import RoutesContainer from './components/RoutesContainer';
-import SmallButton from './components/checkPage/SmallButton';
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
       <BrowserRouter>
-        <SmallButton>확인하기</SmallButton>
         <RoutesContainer />
       </BrowserRouter>
     </ThemeProvider>
