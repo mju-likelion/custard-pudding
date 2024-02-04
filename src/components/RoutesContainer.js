3121f0d66b5443a48ff2ca18abfcafb299e1195c
zelkovaria <hyein396@gmail.com>
2024-02-05 02:59:56 +0900

    feat: Apply 페이지 라우팅 설정 - #16

--- src/components/RoutesContainer.js
+++ src/components/RoutesContainer.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import PageLayout from './PageLayout';
 import Main from '../pages/Main';
+import Apply from '../pages/Apply';
 
 const RoutesContainer = () => {
   return (
     <PageLayout>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/apply" element={<Apply />} />
       </Routes>
     </PageLayout>
   );
