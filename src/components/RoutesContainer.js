765cb2e9620a0b12ff6b7c74d70974cdfb722a2a
zelkovaria <hyein396@gmail.com>
2024-02-05 19:23:53 +0900

    feat: Check 페이지 라우팅 설정 - #16

--- src/components/RoutesContainer.js
+++ src/components/RoutesContainer.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import PageLayout from './PageLayout';
 import Main from '../pages/Main';
 import Apply from '../pages/Apply';
+import Check from '../pages/Check';
 
 const RoutesContainer = () => {
   return (
@@ -9,6 +10,7 @@ const RoutesContainer = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/apply" element={<Apply />} />
+        <Route path="/apply/exist/:id" element={<Check />} />
       </Routes>
     </PageLayout>
   );
