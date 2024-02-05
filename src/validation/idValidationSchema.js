e4b669372522f8ab36217944beee8c32e695108b
zelkovaria <hyein396@gmail.com>
2024-02-05 22:16:51 +0900

    fix: 학번에 대한 유효성 검사로 변경 - #16

--- src/validation/idValidationSchema.js
+++ src/validation/idValidationSchema.js
@@ -3,8 +3,8 @@ import * as yup from 'yup';
 export const idValidationSchema = yup.object({
   id: yup
     .string()
-    .matches(/^[a-zA-Z][0-9a-zA-Z]*$/, '형식에 맞는 아이디를 입력해주세요')
-    .required('아이디를 입력해주세요')
-    .min(6, '6글자 이상 12글자 이하로 입력하세요')
-    .max(12, '6글자 이상 12글자 이하로 입력하세요'),
+    .matches(/^[0-9]*$/, '형식에 맞는 학번을 입력해주세요')
+    .required('학번을 입력해주세요')
+    .min(8, '8글자로 입력해주세요')
+    .max(8, '8글자로 입력해주세요'),
 });
