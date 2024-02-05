6689ae9897606baf9d28370506b86eeebcfb82fa
zelkovaria <hyein396@gmail.com>
2024-02-06 00:08:40 +0900

    feat: 학번 정책에 따른 유효성 검사 - #24

--- src/validation/idValidationSchema.js
+++ src/validation/idValidationSchema.js
@@ -3,8 +3,6 @@ import * as yup from 'yup';
 export const idValidationSchema = yup.object({
   id: yup
     .string()
-    .matches(/^[0-9]*$/, '형식에 맞는 학번을 입력해주세요')
-    .required('학번을 입력해주세요')
-    .min(8, '8글자로 입력해주세요')
-    .max(8, '8글자로 입력해주세요'),
+    .matches(/^[6][0][0-9]{6}$/, '형식에 맞는 학번을 입력해주세요')
+    .required('학번을 입력해주세요'),
 });
