fe29e62615c615ca304c8574ece7c273c0d292c7
zelkovaria <hyein396@gmail.com>
2024-03-01 22:06:02 +0900

    setting: react-ga4 패키지 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  release: 'release version',
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
