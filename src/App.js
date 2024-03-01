09f66399db49bd4e2614ede309bb7a3327724937
zelkovaria <hyein396@gmail.com>
2024-03-01 22:44:24 +0900

    feat: App.js에 useRouteTracker 추가 - #107

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Theme } from './styles/Theme';
 import GlobalStyle from './styles/GlobalStyle';
 import RoutesContainer from './components/RoutesContainer';
+import useRouteTracker from './hooks/useRouteTracker';
 
 function App() {
+  useRouteTracker();
+
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
