54c9ef24c797590a984791286d57f7313895ad6b
zelkovaria <hyein396@gmail.com>
2024-03-01 22:14:05 +0900

    feat: index.js에 초기화 코드 작성 - #107

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as Sentry from '@sentry/react';
 import { BrowserTracing } from '@sentry/tracing';
+import ReactGA from 'react-ga4';
 
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
@@ -13,6 +14,9 @@ Sentry.init({
   tracesSampleRate: 1.0,
 });
 
+if (process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID) {
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID);
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
