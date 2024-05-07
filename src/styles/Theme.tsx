a1bce2890301d4c92d299f376e9cb88a40134ebb
zelkovaria <hyein396@gmail.com>
2024-05-07 17:34:55 +0900

    refactor: Theme 파일 삭제 및 device별 size 분리

--- src/styles/Theme.tsx
+++
@@ -1,9 +0,0 @@
-import { css } from 'styled-components';
-
-export const Theme = {
-  devices: {
-    DESKTOP: `(min-width: 1200px)`,
-    TABLET: `(min-width: 600px)`,
-    MOBILE: `(min-width: 390px)`,
-  },
-};
