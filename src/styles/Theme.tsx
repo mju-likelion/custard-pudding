e35dcd756ef5b5f932cef0e26f9dc40abbc78e4f
zelkovaria <hyein396@gmail.com>
2024-05-07 17:29:15 +0900

    refactor: Theme에서 colors 분리

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -1,17 +1,6 @@
 import { css } from 'styled-components';
 
 export const Theme = {
-  colors: {
-    HOVER_BTN: '#FF5172',
-    MAIN_PINK: '#F48096',
-    DISABLE_BTN: '#939393',
-    WHITE_TXT: '#F9F3F3',
-    MODAL_BG: '#3B3B3B',
-    CARD_BG: '#2B2B2B',
-    INPUT_BG: '#242424',
-    PAGE_BG: '#1C1B1A',
-  },
-
   devices: {
     DESKTOP: `(min-width: 1200px)`,
     TABLET: `(min-width: 600px)`,
---
+++ src/styles/colors.tsx
@@ -0,0 +1,25 @@
+const pink = {
+  //MAIN_PINK
+  pink100: '#F48096',
+  //HOVER_BTN
+  pink200: '#FF5172',
+};
+
+const background = {
+  //DISABLE_BTN
+  background100: '#939393',
+  //  MODAL_BG
+  background200: '#3B3B3B',
+  //CARD_BG
+  background300: '#2B2B2B',
+  //INPUT_BG
+  background400: '#242424',
+  //PAGE_BG
+  background500: '#1C1B1A',
+};
+
+const white = {
+  //WHITE_TXT
+  white: '#F9F3F3',
+};
+export const colors = { ...pink, ...background, ...white };
