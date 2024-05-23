c555e54dbdf7700e44edcd6949fb225fbef86c78
zelkovaria <hyein396@gmail.com>
2024-05-23 19:02:48 +0900

    refactor: Input의 styled-component theme 변경

--- src/components/checkPage/Input.tsx
+++ src/components/checkPage/Input.tsx
@@ -1,5 +1,55 @@
 import styled from 'styled-components';
 
+const Caption = styled.p`
+  ${({ theme }) => theme.typographies.smallTxt};
+  text-align: left;
+  margin: 0 0 6px 2px;
+
+  width: ${({ $captionSize }) => $captionSize.width};
+  height: ${({ $captionSize }) => $captionSize.height};
+
+  @media ${({ theme }) => theme.size.desktop} {
+    width: ${({ $captionSize }) => $captionSize.pcWidth};
+    height: ${({ $captionSize }) => $captionSize.pcHeight};
+    ${({ theme }) => theme.typographies.bigTxt};
+    margin: 0 0 4px 2px;
+  }
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  height: ${({ $inputSize }) => $inputSize.height};
+  background: transparent;
+  border: 1px solid #d4d4d8;
+  ${({ theme }) => theme.typographies.defaultTxt};
+  color: ${({ theme }) => theme.colors.white};
+  padding: 18px 16px;
+  border-radius: 8px;
+
+  @media ${({ theme }) => theme.size.desktop} {
+    width: ${({ $inputSize }) => $inputSize.pcWidth};
+    height: ${({ $inputSize }) => $inputSize.pcHeight};
+  }
+`;
+
+const ErrorMessage = styled.p`
+  width: ${({ $messageSize }) => $messageSize.width};
+  height: ${({ $messageSize }) => $messageSize.height};
+  color: ${({ theme }) => theme.colors.pink200};
+  ${({ theme }) => theme.typographies.smallErrorTxt};
+  margin-top: 7px;
+  margin-left: 4px;
+
+  @media ${({ theme }) => theme.size.desktop} {
+    width: ${({ $messageSize }) => $messageSize.pcWidth};
+    height: ${({ $messageSize }) => $messageSize.pcHeight};
+    ${({ theme }) => theme.typographies.bigTxt};
+    margin-top: 10px;
+  }
+`;
+
+const Wrapper = styled.div``;
+
 const Input = ({
   inputSize,
   captionSize,
@@ -17,55 +67,5 @@ const Input = ({
     </Wrapper>
   );
 };
-
-const Caption = styled.p`
-  ${({ theme }) => theme.typographies.SMALL_TXT};
-  text-align: left;
-  margin: 0 0 6px 2px;
-
-  width: ${({ $captionSize }) => $captionSize.width};
-  height: ${({ $captionSize }) => $captionSize.height};
-
-  @media ${({ theme }) => theme.devices.DESKTOP} {
-    width: ${({ $captionSize }) => $captionSize.pcWidth};
-    height: ${({ $captionSize }) => $captionSize.pcHeight};
-    ${({ theme }) => theme.typographies.BIG_TXT};
-    margin: 0 0 4px 2px;
-  }
-`;
-
-const StyledInput = styled.input`
-  width: 100%;
-  height: ${({ $inputSize }) => $inputSize.height};
-  background: transparent;
-  border: 1px solid #d4d4d8;
-  ${({ theme }) => theme.typographies.DEFAULT_TXT};
-  color: ${({ theme }) => theme.colors.WHITE_TXT};
-  padding: 18px 16px;
-  border-radius: 8px;
-
-  @media ${({ theme }) => theme.devices.DESKTOP} {
-    width: ${({ $inputSize }) => $inputSize.pcWidth};
-    height: ${({ $inputSize }) => $inputSize.pcHeight};
-  }
-`;
-
-const ErrorMessage = styled.p`
-  width: ${({ $messageSize }) => $messageSize.width};
-  height: ${({ $messageSize }) => $messageSize.height};
-  color: ${({ theme }) => theme.colors.HOVER_BTN};
-  ${({ theme }) => theme.typographies.SMALL_ERROR_TXT};
-  margin-top: 7px;
-  margin-left: 4px;
-
-  @media ${({ theme }) => theme.devices.DESKTOP} {
-    width: ${({ $messageSize }) => $messageSize.pcWidth};
-    height: ${({ $messageSize }) => $messageSize.pcHeight};
-    ${({ theme }) => theme.typographies.BIG_TXT};
-    margin-top: 10px;
-  }
-`;
-
-const Wrapper = styled.div``;
 
 export default Input;
