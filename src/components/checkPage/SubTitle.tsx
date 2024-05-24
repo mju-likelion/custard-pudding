ebf5a98fb1ccac59655dd174c7bf76f2eff79e39
zelkovaria <hyein396@gmail.com>
2024-05-24 19:43:43 +0900

    refactor: SubTitle component의 theme 변경

--- src/components/checkPage/SubTitle.tsx
+++ src/components/checkPage/SubTitle.tsx
@@ -9,12 +9,12 @@ const SubTitle = ({ children }) => {
 };
 
 const TitleBox = styled.div`
-  ${({ theme }) => theme.typographies.DEFAULT_TXT};
+  ${({ theme }) => theme.typographies.defaultTxt};
   border-bottom: 1px solid;
   padding-bottom: 2px;
 
-  @media ${({ theme }) => theme.devices.DESKTOP} {
-    ${({ theme }) => theme.typographies.TITLE}
+  @media ${({ theme }) => theme.size.desktop} {
+    ${({ theme }) => theme.typographies.title}
     border-bottom: 2px solid;
     padding-bottom: 4px;
   }
